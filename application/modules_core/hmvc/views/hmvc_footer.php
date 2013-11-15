289d4b5cc77ebc88074b2821bf46f0acbfebc19f
DEF <defsteeldevelopment@gmail.com>
2013-11-15 00:31:40 -0600

    x

--- application/modules_core/hmvc/views/hmvc_footer.php
+++ application/modules_core/hmvc/views/hmvc_footer.php
@@ -1,3 +1,13 @@
+
+<footer>
+<div class="container-narrow" style="text-align: center;">
+    <p><sub><?php echo anchor('public/market', 'HOME'); ?> | <?php echo anchor('public/market/about', 'ABOUT'); ?> | <?php echo anchor('public/market/contact', 'CONTACT'); ?> | <?php echo anchor('public/cart', 'REGISTER'); ?> | <?php echo anchor('public/support', 'SUPPORT'); ?></sub></p>
+    <p><sub>&COPY; 2012 BRAND &TRADE; All Rights reserved</sub></p>
+    <p><sub>Page rendered in <strong>{elapsed_time}</strong> seconds</sub></p>
+</div>
+</footer>
+
+
 <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
